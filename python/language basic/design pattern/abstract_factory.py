e28185768098720394c596819f763231170037cd
Shuo Zhao <shuozhaous@gmail.com>
2015-10-12 20:40:59 -0700

    example for abstract factory

--- python/language basic/design pattern/abstract_factory.py
+++ python/language basic/design pattern/abstract_factory.py
@@ -1,86 +1,156 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import abc
+
+
+class Switch (object):
+    def __init__(self, value):
+        self.value = value
+        self.fall = False
+
+    def __iter__(self):
+        """Return the match method once, then stop"""
+        yield self.match
+        raise StopIteration
+
+    def match(self, *args):
+        """Indicate whether or not to enter a case suite"""
+        if self.fall or not args:
+            return True
+        elif self.value in args: # changed for v1.5, see below
+            self.fall = True
+            return True
+        else:
+            return False
+
 
 # Intent
 # Provide an interface for creating families of related or dependent objects without specifying their concrete classes
 
 # AbstractFactory
 
+class DbAdminFactory(object):
+    __metaclass__ = abc.ABCMeta
 
-class MexicanRestaurants:
-    def make_burrito(self):
+    @abc.abstractmethod
+    def get_data_provider(self):
         return
 
-    def order_taco(self):
+    @abc.abstractmethod
+    def get_configuration_helper(self):
         return
 
 # ConcreteFactory
 
 
-class CovinaFoodTruck(MexicanRestaurants):
-    def order_burrito(self):
-        return
+class MySQLAdminFactory(DbAdminFactory):
+    def get_data_provider(self):
+        return MySQLDataProvider()
 
-    def order_taco(self):
-        return
+    def get_configuration_helper(self):
+        return MySQLConfigurationHelper()
 
 
-class ElFarolito(MexicanRestaurants):
-    def order_burrito(self):
-        return
+class OracleAdminFactory(DbAdminFactory):
+    def get_data_provider(self):
+        return OracleDataProvider()
 
-    def order_taco(self):
-        return
+    def get_configuration_helper(self):
+        return OracleConfigurationHelper()
 
 # AbstractProduct
 
 
-class FoodProduct:
-    def prepare(self):
+class DbDataProvider(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def get_connection(self, connection_string):
         return
 
-    def wrap(self):
+
+class DbConfigurationHelper(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def print_manual(self):
         return
 
-    def box(self):
+    @abc.abstractmethod
+    def change_default_engine(self):
         return
 
 # ConcreteProduct
 
 
-class Burrito(FoodProduct):
-    def prepare(self):
-        return
-
-    def wrap(self):
-        return
-
-    def box(self):
+class MySQLDataProvider(DbDataProvider):
+    def get_connection(self, connection_string):
+        print("MySQL: Trying to connect", connection_string)
         return
 
 
-class Taco(FoodProduct):
-    def prepare(self):
+class MySQLConfigurationHelper(DbConfigurationHelper):
+    def print_manual(self):
+        print("MySQL: Help")
         return
 
-    def wrap(self):
+    def change_default_engine(self):
+        print("MySQL: Change Engine to")
         return
 
-    def box(self):
+
+class OracleDataProvider(DbDataProvider):
+    def get_connection(self, connection_string):
+        print("Oracle: Trying to connect", connection_string)
         return
 
+
+class OracleConfigurationHelper(DbConfigurationHelper):
+    def print_manual(self):
+        print("Oracle: Help")
+        return
+
+    def change_default_engine(self):
+        print("Oracle: Change Engine to")
+        return
 
 
 # Client
 
 
-class Burritophile:
-    def __init__(self,MexicanRestaurants = None):
-        self.restaurant = MexicanRestaurants
+class DatabaseClient:
+    def __init__(self, database_desc):
+        for case in Switch(database_desc):
+            if case('mysql'):
+                self.database = MySQLAdminFactory()
+                break
+            if case('Oracle'):
+                self.database = OracleAdminFactory()
+                break
 
-    def order_food(self):
+        self.provider = self.database.get_data_provider()
+        self.helper = self.database.get_configuration_helper()
+
+    def connect(self, connection_string):
+        self.provider.get_connection(connection_string)
+
+    def help(self):
+        self.helper.print_manual()
+
+    def change_engine(self):
+        self.helper.change_default_engine()
 
 
+if __name__ == "__main__":
+    mysql_client = DatabaseClient('mysql')
+    oracle_client = DatabaseClient('Oracle')
+    mysql_client.connect('angelapper@mysql')
+    oracle_client.connect('angelapper@Oracle')
+    mysql_client.help()
+    oracle_client.help()
 
-
-
+# OUTPUT ###
+# MySQL: Trying to connect angelapper@mysql
+# Oracle: Trying to connect angelapper@Oracle
+# MySQL: Help
+# Oracle: Help
